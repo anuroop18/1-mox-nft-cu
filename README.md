ea124f29a2aac94724d3feeb1af80f5f5d9b3aab
anuroop18 <anuroopsingh945@gmail.com>
2024-12-29 10:39:41 +0000

    changes

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 1. Basic NFT
 2. One where the mood can be changed, and the image be changed.
 
+To check test coverage of each line in html format:
 `mox test --coverage --cov-report html` 
--- tests/test_mood_nft.py
+++ tests/test_mood_nft.py
@@ -64,36 +64,3 @@ def test_token_uri(mood_nft, aryan):
     with open("images/sad.svg", "r") as file:
         sad_svg = file.read()
     assert svg_content == sad_svg
-
-
-# def test_token_transfers(basic_nft, aryan, raghav):
-#     with boa.env.prank(aryan):
-#         basic_nft.mint(PUG_URI)
-#     with boa.env.prank(aryan):
-#         basic_nft.transferFrom(aryan, raghav, 0)
-#     assert basic_nft.ownerOf(0) == raghav
-#     assert basic_nft.balanceOf(aryan) == 0
-#     assert basic_nft.balanceOf(raghav) == 1
-
-# def test_approve_and_transfer(basic_nft, aryan, raghav, chikapa):
-#     with boa.env.prank(aryan):
-#         basic_nft.mint(PUG_URI)
-#     assert basic_nft.ownerOf(0) == aryan
-#     with boa.env.prank(aryan):
-#         basic_nft.approve(chikapa, 0)
-#     assert basic_nft.getApproved(0) == chikapa
-#     with boa.env.prank(chikapa):
-#         basic_nft.transferFrom(aryan, raghav, 0)
-#     assert basic_nft.ownerOf(0) == raghav
-#     assert basic_nft.totalSupply() == 1
-#     assert basic_nft.balanceOf(aryan) == 0
-#     assert basic_nft.balanceOf(raghav) == 1
-#     assert basic_nft.balanceOf(chikapa) == 0
-
-# def test_mint_multiple_tokens(basic_nft, aryan):
-#     with boa.env.prank(aryan):
-#         for i in range(3):
-#             basic_nft.mint(PUG_URI)
-#             assert basic_nft.tokenURI(i) == "ipfs://" + PUG_URI
-#     assert basic_nft.balanceOf(aryan) == 3
-#     assert basic_nft.totalSupply() == 3
